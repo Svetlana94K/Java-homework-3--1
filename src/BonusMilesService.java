145591efc38be4d29a37ec2c31a8a5bb5520fa1d
Neto test <svietlana.kabanova@bk.ru>
2024-10-14 15:44:31 +1100

    Code change

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,12 +1,6 @@
 public class BonusMilesService {
     public int calculate(int price) {
-        int result;
-        if (price > 19) {
-            result = 1;
-        } else {
-            result = 0;
-        }
-        return result;
+        return price / 20;
 
     }
 }
